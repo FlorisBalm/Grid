fe8a19d5db2b3cd746887a02abc2a2ba0fcd52ea
Floris Balm <s1205916@sms.ed.ac.uk>
2017-03-03 16:14:45 +0000

    Added rhorho support, however grid isn't buidling it

---
+++ extras/Hadrons/Modules/MContraction/RhoRho.hpp
@@ -0,0 +1,137 @@
+/*************************************************************************************
+
+Grid physics library, www.github.com/paboyle/Grid 
+
+Source file: extras/Hadrons/Modules/MContraction/RhoRho.hpp
+
+Copyright (C) 2015
+Copyright (C) 2016
+
+Author: Antonin Portelli <antonin.portelli@me.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+See the full license in the file "LICENSE" in the top level distribution directory
+*************************************************************************************/
+/*  END LEGAL */
+
+#ifndef Hadrons_RhoRho_hpp_
+#define Hadrons_RhoRho_hpp_
+
+#include <Grid/Hadrons/Global.hpp>
+#include <Grid/Hadrons/Module.hpp>
+#include <Grid/Hadrons/ModuleFactory.hpp>
+
+BEGIN_HADRONS_NAMESPACE
+
+/******************************************************************************
+ *                                TRhoRho                                       *
+ ******************************************************************************/
+BEGIN_MODULE_NAMESPACE(MContraction)
+
+class RhoRhoPar: Serializable
+{
+public:
+    GRID_SERIALIZABLE_CLASS_MEMBERS(RhoRhoPar,
+                                    std::string, q1,
+                                    std::string, q2,
+                                    std::string,    output);
+};
+
+template <typename FImpl1, typename FImpl2>
+class TRhoRho: public Module<RhoRhoPar>
+{
+public:
+    TYPE_ALIASES(FImpl1, 1);
+    TYPE_ALIASES(FImpl2, 2);
+    class Result: Serializable
+    {
+    public:
+        GRID_SERIALIZABLE_CLASS_MEMBERS(Result, std::vector<Complex>, corr);
+    };
+public:
+    // constructor
+    TRhoRho(const std::string name);
+    // destructor
+    virtual ~TRhoRho(void) = default;
+    // dependencies/products
+    virtual std::vector<std::string> getInput(void);
+    virtual std::vector<std::string> getOutput(void);
+    // execution
+    virtual void execute(void);
+};
+
+MODULE_REGISTER_NS(RhoRho, ARG(TRhoRho<FIMPL,FIMPL>), MContraction);
+
+/******************************************************************************
+ *                           TRhoRho implementation                            *
+ ******************************************************************************/
+// constructor /////////////////////////////////////////////////////////////////
+template <typename FImpl1, typename FImpl2>
+TRhoRho<FImpl1,FImpl2>::TRhoRho(const std::string name)
+: Module<RhoRhoPar>(name)
+{}
+
+// dependencies/products ///////////////////////////////////////////////////////
+template <typename FImpl1, typename FImpl2>
+std::vector<std::string> TRhoRho<FImpl1,FImpl2>::getInput(void)
+{
+    std::vector<std::string> input = {par().q1, par().q2};
+    
+    return input;
+}
+
+template <typename FImpl1, typename FImpl2>
+std::vector<std::string> TRhoRho<FImpl1,FImpl2>::getOutput(void)
+{
+    std::vector<std::string> output = {getName()};
+    
+    return output;
+}
+
+// execution ///////////////////////////////////////////////////////////////////
+template <typename FImpl1, typename FImpl2>
+void TRhoRho<FImpl1,FImpl2>::execute(void)
+{
+    LOG(Message) << "Computing meson contractions '" << getName() << "' using"
+                 << " quarks '" << par().q1 << "' and '" << par().q2 << "'"  
+                 << std::endl;
+    XmlWriter             writer(par().output);
+    PropagatorField1      &q1 = *env().template getObject<PropagatorField1>(par().q1);
+    PropagatorField2      &q2 = *env().template getObject<PropagatorField2>(par().q2);
+    Algebra::Gamma        g3 = Algebra::Gamma::GammaZ, g5 = Algebra::Gamma:Gamma5;
+    LatticeComplex        c(env().getGrid()); 
+
+    std::vector<TComplex> buf;
+    Result                result;
+    c = trace(q1*g5*g3*adj(q2)*g5*g3);
+    sliceSum(c,buf,Tp);
+
+
+
+    //Other traces
+    result.corr.resize(buf.size());
+    
+    for (unsigned int t = 0; t < buf[0].size(); ++t)
+    {
+        result.corr[t] = TensorRemove(buf[t]);
+    }
+    write(writer, "rhorho", result);
+}
+END_MODULE_NAMESPACE
+
+END_HADRONS_NAMESPACE
+
+#endif // Hadrons_RhoRho_hpp_
---
+++ tests/hadrons/RhoCorrelator.cc
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ Grid physics library, www.github.com/paboyle/Grid
+ 
+ Source file: tests/hadrons/Test_hadrons_spectrum.cc
+ 
+ Copyright (C) 2015
+ 
+ Author: Antonin Portelli <antonin.portelli@me.com>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
+ See the full license in the file "LICENSE" in the top level distribution
+ directory.
+ *******************************************************************************/
+
+#include <Grid/Hadrons/Application.hpp>
+#include <Grid/Hadrons/Modules/MSource/Z2.hpp>
+#include <Grid/Hadrons/Modules/MContraction/RhoRho.hpp>
+using namespace Grid;
+using namespace Hadrons;
+
+int main(int argc, char *argv[])
+{
+    // initialization //////////////////////////////////////////////////////////
+    Grid_init(&argc, &argv);
+    HadronsLogError.Active(GridLogError.isActive());
+    HadronsLogWarning.Active(GridLogWarning.isActive());
+    HadronsLogMessage.Active(GridLogMessage.isActive());
+    HadronsLogIterative.Active(GridLogIterative.isActive());
+    HadronsLogDebug.Active(GridLogDebug.isActive());
+    LOG(Message) << "Grid initialized" << std::endl;
+    
+    // run setup ///////////////////////////////////////////////////////////////
+    Application              application;
+
+    // global parameters
+
+    Application::GlobalPar globalPar;
+    globalPar.trajCounter.start = 3425;
+    globalPar.trajCounter.end   = 3495;
+    globalPar.trajCounter.step  = 5;
+    globalPar.seed              = "1 4 3 2";
+    
+    application.setPar(globalPar);
+    // gauge field
+    MGauge::Load::Par loadPar;
+    loadPar.file = "/home/floris/mphys/configurations/ckpoint_lat";
+    application.createModule<MGauge::Load>("gauge", loadPar);
+    
+
+    double mass = 0.1;
+    
+    MSource::Z2::Par z2par;
+    z2par.tA=0;
+    z2par.tB=0;
+    application.createModule<MSource::Z2>("Z2", z2par);
+
+    //Wilson Action
+    MAction::Wilson::Par actionPar;
+    actionPar.gauge = "gauge";
+    actionPar.mass  = mass;
+    application.createModule<MAction::Wilson>("Wilson", actionPar);
+
+    // solvers
+    MSolver::RBPrecCG::Par solverPar;
+    solverPar.action   = "Wilson";
+    solverPar.residual = 1.0e-8;
+    application.createModule<MSolver::RBPrecCG>("CG",solverPar);
+
+    // propagators
+    Quark::Par quarkPar1;
+    quarkPar1.solver = "CG";
+    quarkPar1.source = "Z2";
+    application.createModule<Quark>("QZ2_1", quarkPar1);
+
+    Quark::Par quarkPar2;
+    quarkPar2.solver = "CG";
+    quarkPar2.source = "Z2";
+    application.createModule<Quark>("QZ2_2", quarkPar2);
+
+
+    time_t t = time(0);
+    struct tm* now = localtime(&t);
+    
+    char buffer[80];
+    strftime(buffer, 80, "%Y%m%d",now);
+    std::string current_date(buffer);
+    strftime(buffer, 80, "%H%M%S",now);
+    std::string current_time(buffer); 
+
+    MContraction::RhoRho::Par rhoPar;
+    rhoPar.q1 = "QZ2_1";
+    rhoPar.q2 = "QZ2_1";
+    rhoPar.output= "rhorho/"+current_date+"/Z2_"+current_time;
+
+    application.createModule<MContraction::RhoRho>("RhoRho_Z2",
+            rhoPar);
+
+    // execution
+    application.saveParameterFile("RhoRhoZ2.xml");
+    application.run();
+
+    // epilogue
+    LOG(Message) << "Grid is finalizing now" << std::endl;
+    Grid_finalize();
+
+    return EXIT_SUCCESS;
+}
